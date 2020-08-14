83a72550159e5173dfcfc676457690918991cdef
Peter Onyisi <ponyisi@utexas.edu>
2020-08-14 14:24:53 +0200

    Improve test

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,12 +1,15 @@
 def test_run():
     import subprocess
-    chk = subprocess.run("python -m histgrinder.make_sample_file", shell=True)
-    if chk:
-        return False
-    chk = subprocess.run("wget https://github.com/ponyisi/histogram_postprocessing/blob/master/resources/example.yaml",
-                         shell=True)
-    if chk:
-        return False
+    chk = subprocess.run("python -m histgrinder.make_sample_file",
+                         shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    print(chk.stdout)
+    chk.check_returncode()
+    chk = subprocess.run("wget https://raw.githubusercontent.com/ponyisi/histogram_postprocessing/master/resources/example.yaml",
+                         shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    print(chk.stdout)
+    chk.check_returncode()
     chk = subprocess.run("python -m histgrinder.engine example.root example.root -c example.yaml --prefix prefix",
-                         shell=True)
-    return chk == 0
+                         shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    print(chk.stdout)
+    chk.check_returncode()
+    return True
