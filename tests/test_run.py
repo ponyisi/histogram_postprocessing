286d59b7a3d5f018487b90ec9abc5b753fedd822
Peter Onyisi <ponyisi@utexas.edu>
2020-08-14 04:51:14 +0200

    Add shell= option

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,8 +1,8 @@
 def test_run():
     import subprocess
-    chk = subprocess.run("python -m histgrinder.make_sample_file")
+    chk = subprocess.run("python -m histgrinder.make_sample_file", shell=True)
     if chk: return False
-    chk = subprocess.run("wget https://github.com/ponyisi/histogram_postprocessing/blob/master/resources/example.yaml")
+    chk = subprocess.run("wget https://github.com/ponyisi/histogram_postprocessing/blob/master/resources/example.yaml", shell=True)
     if chk: return False
-    chk = subprocess.run("python3 -m histgrinder.engine example.root example.root -c example.yaml --prefix prefix")
+    chk = subprocess.run("python3 -m histgrinder.engine example.root example.root -c example.yaml --prefix prefix", shell=True)
     return chk == 0
