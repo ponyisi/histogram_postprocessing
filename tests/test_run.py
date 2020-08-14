66186b863e00830b2f25d261560b6d20b970fec2
Peter Onyisi <ponyisi@utexas.edu>
2020-08-14 04:48:13 +0200

    Add test

---
+++ tests/test_run.py
@@ -0,0 +1,8 @@
+def test_run():
+    import subprocess
+    chk = subprocess.run("python -m histgrinder.make_sample_file")
+    if chk: return False
+    chk = subprocess.run("wget https://github.com/ponyisi/histogram_postprocessing/blob/master/resources/example.yaml")
+    if chk: return False
+    chk = subprocess.run('python3 -m histgrinder.engine example.root example.root -c example.yaml --prefix prefix")
+    return chk == 0
