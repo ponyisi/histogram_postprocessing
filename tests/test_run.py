c2c440314480d71af1f7fbe30edbab34a04179b3
Peter Onyisi <ponyisi@utexas.edu>
2020-09-01 16:55:50 +0200

    Improve tests to catch if we don't write histograms to output

--- tests/test_run.py
+++ tests/test_run.py
@@ -10,7 +10,7 @@ def test_run():
                          shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     print(chk.stdout)
     chk.check_returncode()
-    chk = subprocess.run("wget https://raw.githubusercontent.com/ponyisi/histogram_postprocessing/master/resources/example.yaml",  # noqa: E501
+    chk = subprocess.run("wget https://raw.githubusercontent.com/ponyisi/histogram_postprocessing/master/resources/example.yaml -O example.yaml",  # noqa: E501
                          shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     print(chk.stdout)
     chk.check_returncode()
@@ -18,4 +18,12 @@ def test_run():
                          shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     print(chk.stdout)
     chk.check_returncode()
+
+    # verify contents
+    f = ROOT.TFile.Open("example.root")
+    d = f.Get("prefix")
+    # did we create new histograms?
+    assert len(d.GetListOfKeys()) == 31
+    # correct size?
+    assert f.Get("prefix/gauRMS").GetEntries() == 100
     return True
