d412e630231d64a70286b82e8e50d69276a9dd4e
Peter Onyisi <ponyisi@utexas.edu>
2022-04-03 21:12:34 +0200

    Add test for full match

--- tests/test_run.py
+++ tests/test_run.py
@@ -214,3 +214,26 @@ def test_run_badreturn():
     assert b'Function histgrinder.example.nop gave 0 return values but the YAML configuration specifies 1' in chk.stdout
     with pytest.raises(CalledProcessError):
         chk.check_returncode()
+
+
+def test_run_fullmatch():
+    ROOT = pytest.importorskip("ROOT")
+
+    import subprocess
+    chk = subprocess.run("python -m histgrinder.make_sample_file",
+                         shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    print(chk.stdout)
+    chk.check_returncode()
+    chk = subprocess.run("python -m histgrinder.engine example.root new.root "
+                         "-c tests/test_fullmatch.yaml --prefix prefix",
+                         shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    print(chk.stdout)
+    chk.check_returncode()
+
+    # verify output
+    f = ROOT.TFile.Open("new.root")
+    d = f.Get('prefix')
+    assert d
+    # correct size? Should only be 10, not 100!
+    assert f.Get("prefix/gauRMS").GetEntries() == 10
+    return True
